ca5d71e02e830bbe2cb13ea7204286fad15b454e
KorneliyaB <korneliyabanyas@gmail.com>
2023-04-18 13:29:02 -0500

    Initial version

--- src/class10/HW/Task6.java
+++ src/class10/HW/Task6.java
@@ -6,6 +6,35 @@ public class Task6 {
      that array using 2 different loops and calculate how many total countries been stored.
      */
     public static void main(String[] args) {
+        String [][] countries={
+                {"Mexico","USA","Canada"}, //north america countries
+                {"Cuba","Panama","Venezuela"}, //south america countries
+                {"Spain","France","Italy"}, //europe countries
+                {"Japan","China","South Korea"}, //asian countries
+                {"Nigeria","Ethiopia","Angola","Uganda"} //african countries
+        };
+        int sum=0;
+        for (int i=0;i< countries.length; i++){
+            for (int j = 0; j < countries[i].length; j++) {
+                System.out.print(countries[i][j]+" ");
+                sum++;
+
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        int sum2=0;
+        for (String[]country:countries){
+            for (String c:country){
+                System.out.print(c+" ");
+                sum2++;
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Total countries - "+ sum2);
+
 
 
     }
