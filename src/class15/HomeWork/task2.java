df27563216fbb3ee44c4fa6f965cc7afacd730c3
KorneliyaB <130500752+KorneliyaB@users.noreply.github.com>
2023-05-01 13:47:28 -0500

    Delete task2.java

--- src/class15/HomeWork/task2.java
+++
@@ -1,9 +0,0 @@
-package class15.HomeWork;
-
-public class task2 {
-    //Create a Class called Students
-    //Create three  variables  Name , ID  and  numberOfStudents
-    //Create three objects of the Students Class
-    //Set the value for  studentName , studentID and increment  the numberOfStudents for each object
-    //Print out  total number of students
-}
